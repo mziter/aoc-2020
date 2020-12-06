1ec34fb35f0ca945c2c6730f5f1fb356cb333f6e
Matthew Ziter <matt@Matthews-MBP.localdomain>
2020-12-06 09:35:48 -0500

    Added tests for day six

--- day/six/six_test.go
+++ day/six/six_test.go
@@ -2,8 +2,40 @@ package six
 
 import "testing"
 
-func TestSomething(t *testing.T) {
-	if 1 != 1 {
-		t.Error("something is very wrong...")
+func TestCountDistinctChars(t *testing.T) {
+	cases := []struct {
+		input  []string
+		output int
+	}{
+		{[]string{"abc"}, 3},
+		{[]string{"a", "b", "c"}, 3},
+		{[]string{"ab", "bc"}, 3},
+		{[]string{"a", "a", "a", "a"}, 1},
+		{[]string{"b"}, 1},
+	}
+	for _, c := range cases {
+		res := countDistinctChars(c.input)
+		if res != c.output {
+			t.Errorf("Expected result of input %s, to be %d, but was %d", c.input, c.output, res)
+		}
+	}
+}
+
+func TestCountAllYes(t *testing.T) {
+	cases := []struct {
+		input  []string
+		output int
+	}{
+		{[]string{"abc"}, 3},
+		{[]string{"a", "b", "c"}, 0},
+		{[]string{"ab", "bc"}, 1},
+		{[]string{"a", "a", "a", "a"}, 1},
+		{[]string{"b"}, 1},
+	}
+	for _, c := range cases {
+		res := countAllYes(c.input)
+		if res != c.output {
+			t.Errorf("Expected result of input %s, to be %d, but was %d", c.input, c.output, res)
+		}
 	}
 }
